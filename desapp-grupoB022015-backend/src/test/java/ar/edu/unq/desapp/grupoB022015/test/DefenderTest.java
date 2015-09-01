64c0e3dce5644da7d9604f440916985635708a39
JuanManuelVallejos <juanmanuelvallejos2@gmail.com>
2015-09-01 18:17:34 -0300

    insert the first test

--- desapp-grupoB022015-backend/src/test/java/ar/edu/unq/desapp/grupoB022015/test/DefenderTest.java
+++ desapp-grupoB022015-backend/src/test/java/ar/edu/unq/desapp/grupoB022015/test/DefenderTest.java
@@ -1,14 +1,23 @@
 package ar.edu.unq.desapp.grupoB022015.test;
-import static org.junit.Assert.*;
-
+import org.junit.Before;
 import org.junit.Test;
+
+import ar.edu.unq.desapp.grupoB022015.model.Defender;
 
 
 public class DefenderTest {
+	
+	
+	private Defender defender;
 
+	@Before
+    public void init() {
+      defender = new Defender();
+    }
+	
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		assert(defender.calculatePoints() == 3);
 	}
 
 }
