2e7318161faadc56624cdf77b7b8e633264e14b0
JuanManuelVallejos <juanmanuelvallejos2@gmail.com>
2015-09-01 18:32:50 -0300

    travis test

--- desapp-grupoB022015-backend/src/test/java/ar/edu/unq/desapp/grupoB022015/test/DefenderTest.java
+++ desapp-grupoB022015-backend/src/test/java/ar/edu/unq/desapp/grupoB022015/test/DefenderTest.java
@@ -1,8 +1,5 @@
 package ar.edu.unq.desapp.grupoB022015.test;
-import org.junit.Before;
-import org.junit.Test;
 
-import ar.edu.unq.desapp.grupoB022015.model.Defender;
 
 
 public class DefenderTest {
