0519feb3ffd237197109d964e3989c46cda504f8
JuanManuelVallejos <juanmanuelvallejos2@gmail.com>
2015-09-01 18:36:01 -0300

    travis Test

--- desapp-grupoB022015-backend/src/test/java/ar/edu/unq/desapp/grupoB022015/test/DefenderTest.java
+++ desapp-grupoB022015-backend/src/test/java/ar/edu/unq/desapp/grupoB022015/test/DefenderTest.java
@@ -1,12 +1,15 @@
 package ar.edu.unq.desapp.grupoB022015.test;
+import org.junit.Test;
 
+import ar.edu.unq.desapp.grupoB022015.model.Defender;
 
 
 public class DefenderTest {
 	
-
+	
+	@Test
 	public void test() {
-
+		assert(new Defender().calculatePoints() == 3);
 	}
 
 }
