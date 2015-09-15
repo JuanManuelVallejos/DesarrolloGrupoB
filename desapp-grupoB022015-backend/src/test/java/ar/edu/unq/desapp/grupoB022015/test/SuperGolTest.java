c1f6bb363e425c68932011cc3b3ff6b94ed5052a
JuanManuelVallejos <juanmanuelvallejos2@gmail.com>
2015-09-15 17:13:45 -0300

    delete wrongs gitignore

---
+++ desapp-grupoB022015-backend/src/test/java/ar/edu/unq/desapp/grupoB022015/test/SuperGolTest.java
@@ -0,0 +1,48 @@
+package ar.edu.unq.desapp.grupoB022015.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import ar.edu.unq.desapp.grupoB022015.model.Player;
+import ar.edu.unq.desapp.grupoB022015.model.RealTeam;
+import ar.edu.unq.desapp.grupoB022015.model.SuperGol;
+import ar.edu.unq.desapp.grupoB022015.model.User;
+
+public class SuperGolTest {
+
+	
+	private SuperGol supergol;
+	
+	@Before
+	public void init(){
+		supergol =  new SuperGol();
+	}
+	
+	@Test
+	public void addRealTeamTest() {
+		supergol.addRealTeam("Boca Juniors");
+		assertEquals(supergol.getRealTeams().size(),1);
+		assertEquals(supergol.getTeamWithName("River") ,null);
+	}
+	
+	@Test
+	public void addUserTest(){
+		User user = Mockito.mock(User.class);
+		supergol.addUser(user);
+		assertEquals(supergol.getUsers().get(0),user);
+	}
+	
+	@Test
+	public void addPlayerToRealTeam(){
+		supergol.addRealTeam("Independiente");
+		Player player = Mockito.mock(Player.class);
+		supergol.addPlayerTo(player, "Independiente");
+		RealTeam indep = supergol.getTeamWithName("Independiente");
+		assertEquals(indep.getPlayers().size() , 1);
+		assertEquals(indep.getPlayers().get(0),player);
+	}
+
+}
