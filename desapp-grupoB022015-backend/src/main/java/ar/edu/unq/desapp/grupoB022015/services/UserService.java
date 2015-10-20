495b499ee02f39831a1b87f6fe2cbec5c82cd6f8
JuanManuelVallejos <juanmanuelvallejos2@gmail.com>
2015-10-20 17:03:21 -0300

    user configuration

---
+++ desapp-grupoB022015-backend/src/main/java/ar/edu/unq/desapp/grupoB022015/services/UserService.java
@@ -0,0 +1,14 @@
+package ar.edu.unq.desapp.grupoB022015.services;
+
+import ar.edu.unq.desapp.grupoB022015.model.User;
+
+public class UserService extends GenericService<User> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6143091016226478860L;
+
+	
+	
+}
