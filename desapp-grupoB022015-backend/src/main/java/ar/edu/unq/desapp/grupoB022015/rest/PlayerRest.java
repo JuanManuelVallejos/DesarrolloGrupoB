0866e3b9b6633cbc15725dd3a73fefd0e6da688a
JuanManuelVallejos <juanmanuelvallejos2@gmail.com>
2015-10-20 12:34:14 -0300

    add positions in player

--- desapp-grupoB022015-backend/src/main/java/ar/edu/unq/desapp/grupoB022015/rest/PlayerRest.java
+++ desapp-grupoB022015-backend/src/main/java/ar/edu/unq/desapp/grupoB022015/rest/PlayerRest.java
@@ -12,6 +12,9 @@ import javax.ws.rs.core.Response;
 
 import ar.edu.unq.desapp.grupoB022015.model.Defender;
 import ar.edu.unq.desapp.grupoB022015.model.FantasyTeam;
+import ar.edu.unq.desapp.grupoB022015.model.Forward;
+import ar.edu.unq.desapp.grupoB022015.model.Goalkeeper;
+import ar.edu.unq.desapp.grupoB022015.model.Midfielder;
 import ar.edu.unq.desapp.grupoB022015.model.Player;
 import ar.edu.unq.desapp.grupoB022015.model.Position;
 import ar.edu.unq.desapp.grupoB022015.model.SuperGol;
@@ -42,19 +45,29 @@ public class PlayerRest {
 	@Path("/create")
 	@Produces("application/json")
 	public Response createPlayer(@FormParam("name") String name,
-			 @FormParam("ID") int id){//, @FormParam("position") Position position){
+			 @FormParam("ID") int id, @FormParam("position") String position){
 		
 		
 		Player player = getPlayerService().findById(id);
 		
 		if(player == null){
 			//pasar supergol q corresponde
-			Player p = new Player(new SuperGol(), id, name ,new Defender());//, position);
+			Player p = new Player(new SuperGol(), id, name , getPosition(position));
 			getPlayerService().save(p);
 		}else{
 			return Response.ok(-1).build();
 		}
 		return Response.ok(player).build();
+	}
+
+	private Position getPosition(String position) {
+		if(position == "Goalkeeper")
+			return new Goalkeeper();
+		if(position == "Defender")
+			return new Defender();
+		if(position == "Midfielder")
+			return new Midfielder();
+		return new Forward();
 	}
 
 	@GET
