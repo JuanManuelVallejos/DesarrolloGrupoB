4c665df5927fd9480ef249892127b55593038cbe
luis-zagarella <zagarojo23@gmail.com>
2015-11-27 19:31:47 -0300

    createTeam view changes, uploadRteams view and controller changes

--- desapp-grupoB022015-frontend/app/scripts/controllers/uploadRTeams.js
+++ desapp-grupoB022015-frontend/app/scripts/controllers/uploadRTeams.js
@@ -46,7 +46,29 @@ uploadRTeams.directive('file', function(){
             alert('No se pudieron subir los equipos, error (' + status + ')');
         });
 
-}});
+    }
+    
+    $scope.addPlayer = function() {
+        $http({
+            method: 'POST',
+            url: 'http://localhost:8080/desapp-grupoB022015-backend/rest/realTeam/createPlayer/' + $scope.playerteam + '/' + $scope.playername + '/' + $scope.playerposition,
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+            transformRequest: function(obj) {
+                var str = [];
+                    for(var l in obj)
+                        str.push(encodeURIComponent(l) + "=" + encodeURIComponent(obj[l]));
+            return str.join("&");
+            },
+        }).success(function (data) {
+                alert('El jugador fue agregado satisfactoriamente')
+        }).error(function(data,status) {
+                alert("Error (" + status +"): " + "no se pudo agregar el jugador.");
+                location = '#/';
+        });
+    }
+
+
+    });
 function UploadRTeamsCtrl($scope){
     $scope.param = {};
 }
